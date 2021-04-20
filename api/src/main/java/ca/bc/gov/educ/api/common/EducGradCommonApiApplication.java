450dcffca5eaaa418160942827a34ffa0db1b074
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-20 15:33:57 -0700

    Change to webclient

--- api/src/main/java/ca/bc/gov/educ/api/common/EducGradCommonApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/common/EducGradCommonApiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -33,6 +34,11 @@ public class EducGradCommonApiApplication {
 
 		ModelMapper modelMapper = new ModelMapper();
 		return modelMapper;
+	}
+	
+	@Bean
+	public WebClient webClient() {
+		return WebClient.create();
 	}
 	
 	@Bean
